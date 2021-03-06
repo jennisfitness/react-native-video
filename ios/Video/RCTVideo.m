fba08578b18df6bfe9d16a08f49b609a23897bcb
Nuno Campos <nuno@boringbits.io>
2021-03-06 21:08:06 +0000

    Ensure ignoreSilentSwitch and configureAudio props are respected if set while video is paused

--- ios/Video/RCTVideo.m
+++ ios/Video/RCTVideo.m
@@ -953,12 +953,14 @@ static int const RCTVideoUnset = -1;
 - (void)setIgnoreSilentSwitch:(NSString *)ignoreSilentSwitch
 {
   _ignoreSilentSwitch = ignoreSilentSwitch;
+  [self configureAudio];
   [self applyModifiers];
 }
 
 - (void)setMixWithOthers:(NSString *)mixWithOthers
 {
   _mixWithOthers = mixWithOthers;
+  [self configureAudio];
   [self applyModifiers];
 }
 
@@ -968,29 +970,7 @@ static int const RCTVideoUnset = -1;
     [_player pause];
     [_player setRate:0.0];
   } else {
-    AVAudioSession *session = [AVAudioSession sharedInstance];
-    AVAudioSessionCategory category = nil;
-    AVAudioSessionCategoryOptions options = nil;
-
-    if([_ignoreSilentSwitch isEqualToString:@"ignore"]) {
-      category = AVAudioSessionCategoryPlayback;
-    } else if([_ignoreSilentSwitch isEqualToString:@"obey"]) {
-      category = AVAudioSessionCategoryAmbient;
-    }
-
-    if([_mixWithOthers isEqualToString:@"mix"]) {
-      options = AVAudioSessionCategoryOptionMixWithOthers;
-    } else if([_mixWithOthers isEqualToString:@"duck"]) {
-      options = AVAudioSessionCategoryOptionDuckOthers;
-    }
-
-    if (category != nil && options != nil) {
-      [session setCategory:category withOptions:options error:nil];
-    } else if (category != nil && options == nil) {
-      [session setCategory:category error:nil];
-    } else if (category == nil && options != nil) {
-      [session setCategory:session.category withOptions:options error:nil];
-    }
+    [self configureAudio];
 
     if (@available(iOS 10.0, *) && !_automaticallyWaitsToMinimizeStalling) {
       [_player playImmediatelyAtRate:_rate];
@@ -1149,6 +1129,33 @@ static int const RCTVideoUnset = -1;
   [self setAllowsExternalPlayback:_allowsExternalPlayback];
   [self setExitsFullScreenWhenPlaybackEnds:_exitsFullScreenWhenPlaybackEnds];
 }
+
+- (void)configureAudio
+ {
+  AVAudioSession *session = [AVAudioSession sharedInstance];
+  AVAudioSessionCategory category = nil;
+  AVAudioSessionCategoryOptions options = nil;
+
+  if([_ignoreSilentSwitch isEqualToString:@"ignore"]) {
+    category = AVAudioSessionCategoryPlayback;
+  } else if([_ignoreSilentSwitch isEqualToString:@"obey"]) {
+    category = AVAudioSessionCategoryAmbient;
+  }
+
+  if([_mixWithOthers isEqualToString:@"mix"]) {
+    options = AVAudioSessionCategoryOptionMixWithOthers;
+  } else if([_mixWithOthers isEqualToString:@"duck"]) {
+    options = AVAudioSessionCategoryOptionDuckOthers;
+  }
+
+  if (category != nil && options != nil) {
+    [session setCategory:category withOptions:options error:nil];
+  } else if (category != nil && options == nil) {
+    [session setCategory:category error:nil];
+  } else if (category == nil && options != nil) {
+    [session setCategory:session.category withOptions:options error:nil];
+  }
+ }
 
 - (void)setRepeat:(BOOL)repeat {
   _repeat = repeat;
